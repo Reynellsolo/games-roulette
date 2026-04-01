144ea52278fcf45af20582516c39ef16efc3ef97
Reynellsolo <soloreynell@gmail.com>
2026-04-02 04:34:50 +0500

    Create ImportedOrder.js

---
+++ models/ImportedOrder.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const importedOrderSchema = new mongoose.Schema({
+  gameName: { type: String, default: '' },
+  gameKey: { type: String, default: '' },
+  orderNumber: { type: String, default: '' },
+  customerName: { type: String, default: '' },
+}, { timestamps: true });
+
+importedOrderSchema.index({ customerName: 'text' });
+importedOrderSchema.index({ orderNumber: 1 }, { unique: true, sparse: true });
+
+module.exports = mongoose.model('ImportedOrder', importedOrderSchema);
